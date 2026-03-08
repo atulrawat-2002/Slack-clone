922e39e02498094253725032546cdde58cf73786
Atul <dr6217811@gmail.com>
2026-03-08 22:13:56 +0530

    Added basic routing layer

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 
 import { connectDB } from './configs/dbConfig.js';
 import { PORT } from './configs/serverConfigs.js';
-
+import apiRouter from './routes/apiRoutes.js'
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.status(StatusCodes.OK).json({
---
+++ backend/src/routes/v1/users.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.json({
+        message: '/GET users'
+    })
+})
+
+
+export default router;
