495c383dd98bc5983767667e4a094279b25a0a1a
Atul <dr6217811@gmail.com>
2026-03-11 07:38:28 +0530

    Iplemented the addMemberToWorkSpace method  repository

---
+++ backend/src/repositories/workSpaceRepositories.js
@@ -0,0 +1,68 @@
+import User from "../schemas/user.js";
+import WorkSpace from "../schemas/workSpace.js"
+import crudRepository from "./crudRepository.js"
+
+
+const workSpaceRepositories = {
+    ...crudRepository(WorkSpace),
+    getWorkSpaceByName: async function(workSpaceName) {
+        const workSpace = WorkSpace.findOne({
+            name: workSpaceName
+        });
+
+        if (!workSpace) {
+            throw new Error('Work space not found');
+        }
+
+        return workSpace;
+
+    },
+    getWorkSpaceByJoinCode: async function(joinCode) {
+        const workSpace = WorkSpace.findOne({
+            joinCode: joinCode
+        });
+
+        if (!workSpace) {
+            throw new Error('Work space not found');
+        }
+
+        return workSpace;
+    },
+    addMemberToWorkSpace: async function (workSpaceId, memberId, role){
+        const workSpace = await WorkSpace.findById(workSpaceId);
+
+        if(!workSpace) {
+            throw new Error('Work Space not found');
+        }
+
+        const isValidUser = await User.findById(memberId);
+
+        if (!isValidUser) {
+            throw new Error('No User found')
+        }
+
+        const memberAlreadyExist = workSpace.members.find(
+            (member) => member.memberId === memberId
+        );
+
+        if(memberAlreadyExist) {
+            throw new Error ('User alread present in the workspace');
+        }
+
+        workSpace.members.push({
+            memberId,
+            role
+        })
+
+        await workSpace.save();
+    },
+    addChannelToWorkSpace: async function() {
+
+    },
+    fetchAllWorkSpaceByMemberId: async function () {
+
+    }
+}
+
+
+export default workSpaceRepositories;
